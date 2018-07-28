f2238d985a3d8dc43af4ff6488b7dd6331525685
Petros Politis <polpetros@gmail.com>
2018-07-28 16:37:49 +0300

    initial commit - modal

---
+++ Core.Modal/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Core.Modal.Models;
+
+namespace Core.Modal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            Credentials credentials = new Credentials();
+            ViewData["Message"] = Request.Path.ToString();
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> EnterPin(int id)
+        {
+            Credentials credentials = new Credentials();
+            return PartialView("~/Views/SalesOrders/_FormLine.cshtml", credentials);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EnterPin(Credentials credentials)
+        {
+
+            return Redirect(Request.Path.ToString());
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Core.Modal/Models/Credentials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Modal.Models
+{
+    public class Credentials
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Pin { get; set; }
+        public DateTime SessionStart { get; set; }
+    }
+}
